c0871b242dd3db1bc4601f60a8bfeb738036f8a3
James Rutledge <jarutledge2@gmail.com>
2016-03-28 00:18:58 -0400

    collisions working

--- game.js
+++ game.js
@@ -20,6 +20,13 @@
 
 	Game.prototype = {
 		update: function() {
+			var bodies = this.bodies;
+			var notCollidingWithAnything = function(b1) {
+				return bodies.filter(function(b2) { return colliding(b1, b2); }).length === 0;
+			}
+
+			this.bodies = this.bodies.filter(notCollidingWithAnything);
+
 			for (var i = 0; i < this.bodies.length; i++) {
 				this.bodies[i].update();
 			};
@@ -124,6 +131,14 @@
 		};
 
 		this.KEYS = { LEFT: 37, RIGHT: 39, SPACE: 32 };
+	};
+
+	var colliding = function(b1, b2) {
+		return !(b1 === b2 ||
+				 b1.center.x + b1.size.x / 2 < b2.center.x - b2.size.x / 2 ||
+				 b1.center.y + b1.size.y / 2 < b2.center.y - b2.size.y / 2 ||
+				 b1.center.x - b1.size.x / 2 > b2.center.x + b2.size.x / 2 ||
+				 b1.center.y - b1.size.y / 2 > b2.center.y + b2.size.y / 2);
 	};
 
 	window.onload = function() {
