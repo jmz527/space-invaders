192faaef0a8f962e4dde2cd0d7cb7994a63a6bfa
James Rutledge <jarutledge2@gmail.com>
2016-03-27 23:44:40 -0400

    player movement

--- game.js
+++ game.js
@@ -5,6 +5,9 @@
 		var screen = canvas.getContext('2d');
 		var gameSize = { x: canvas.width, y: canvas.height };
 
+		this.bodies = [new Player(this, gameSize)];
+
+
 		var self = this;
 		var tick = function() {
 			self.update();
@@ -17,12 +20,58 @@
 
 	Game.prototype = {
 		update: function() {
-			console.log('hi');
+			for (var i = 0; i < this.bodies.length; i++) {
+				this.bodies[i].update();
+			};
 		},
 
-		draw: function() {
-
+		draw: function(screen, gameSize) {
+			screen.clearRect(0, 0, gameSize.x, gameSize.y);
+			for (var i = 0; i < this.bodies.length; i++) {
+				drawRect(screen, this.bodies[i]);
+			};
 		}
+	};
+
+	var Player = function(game, gameSize) {
+		this.game = game;
+		this.size = { x: 15, y: 15 };
+		this.center = { x: gameSize.x /2, y: gameSize.y - this.size.x };
+		this.Keyboarder = new Keyboarder();
+	};
+
+	Player.prototype = {
+		update: function() {
+			if (this.Keyboarder.isDown(this.Keyboarder.KEYS.LEFT)) {
+				this.center.x -= 2;
+			} else if (this.Keyboarder.isDown(this.Keyboarder.KEYS.RIGHT)) {
+				this.center.x += 2;
+			}
+		}
+	};
+
+	var drawRect = function(screen, body) {
+		screen.fillRect(body.center.x - body.size.x / 2,
+						body.center.y - body.size.y / 2,
+						body.size.x, body.size.y);
+	};
+
+	var Keyboarder = function() {
+		var keyState = {};
+
+		window.onkeydown = function(e) {
+			keyState[e.keyCode] = true;
+		};
+
+		window.onkeyup = function(e) {
+			keyState[e.keyCode] = false;
+		};
+
+		this.isDown = function(keyCode) {
+			return keyState[keyCode] === true;
+		};
+
+		this.KEYS = { LEFT: 37, RIGHT: 39, SPACE: 32 };
 	};
 
 	window.onload = function() {
